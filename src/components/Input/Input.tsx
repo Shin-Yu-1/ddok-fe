e4e7c55e41a94d5b27c432b481ed71dc5b9dce6d
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-17 15:09:26 +0900

    feat: Input에 props로 받은 스타일이 인라인 스타일로 적용되도록 추가함 / DP-230

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,93 @@
+import { forwardRef } from 'react';
+import styles from './Input.module.scss';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  width?: number | string;
+  height?: number | string;
+  fontSize?: number | string;
+  fontWeight?: number | string;
+  radius?: number | string;
+  padding?: number | string;
+  gap?: number | string;
+  iconSize?: number | string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  focusBorderColor?: string;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  error?: boolean;
+  errorMessage?: string;
+  label?: string;
+}
+
+const toCssSize = (value?: number | string) => (typeof value === 'number' ? `${value}px` : value);
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      width,
+      height,
+      fontSize,
+      fontWeight,
+      radius,
+      padding,
+      gap,
+      iconSize,
+      backgroundColor,
+      textColor,
+      borderColor,
+      focusBorderColor,
+
+      leftIcon,
+      rightIcon,
+
+      disabled,
+      style,
+      ...rest
+    },
+    ref
+  ) => {
+    const inlineStyle: React.CSSProperties = {
+      ...(width ? { width: toCssSize(width) } : null),
+      ...(height ? { height: toCssSize(height) } : null),
+      ...(fontSize ? { fontSize: toCssSize(fontSize) } : null),
+      ...(fontWeight ? { fontWeight: toCssSize(fontWeight) } : null),
+      ...(radius ? { borderRadius: toCssSize(radius) } : null),
+      ...(padding ? { padding: toCssSize(padding) } : null),
+      ...(gap ? { gap: toCssSize(gap) } : null),
+      ...(backgroundColor ? { backgroundColor: backgroundColor } : null),
+      ...(textColor ? { color: textColor } : null),
+      ...(borderColor ? { borderColor } : null),
+      ...(focusBorderColor
+        ? ({ '--focus-border-color': focusBorderColor } as React.CSSProperties)
+        : null),
+      ...style,
+    };
+
+    const inlineIconStyle: React.CSSProperties = {
+      ...(iconSize ? { width: toCssSize(iconSize), height: toCssSize(iconSize) } : null),
+    };
+
+    return (
+      <div className={styles.defaultInputWrapper} style={inlineStyle}>
+        {leftIcon && (
+          <span className={styles.icon} style={inlineIconStyle}>
+            {leftIcon}
+          </span>
+        )}
+
+        <input ref={ref} disabled={disabled} className={styles.defaultInput} {...rest} />
+
+        {rightIcon && (
+          <span className={styles.icon} style={inlineIconStyle}>
+            {rightIcon}
+          </span>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
+export default Input;
