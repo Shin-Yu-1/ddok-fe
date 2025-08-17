efeb1d05b3b422edcef9310ed7e97a975d841834
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-17 19:34:58 +0900

    feat: overflow menu 추가 / DP-230

---
+++ src/components/OverflowMenu/OverflowMenu.tsx
@@ -0,0 +1,32 @@
+import { forwardRef } from 'react';
+import styles from './OverflowMenu.module.scss';
+
+interface OverflowMenuItem {
+  icon?: React.ReactNode;
+  name: string;
+  onClick?: () => void;
+}
+
+interface menuProps {
+  position?: { top?: number; bottom?: number; right?: number; left?: number };
+  menuItems?: OverflowMenuItem[];
+}
+
+const OverflowMenu = forwardRef<HTMLDivElement, menuProps>(
+  ({ position = { top: 0, left: 0 }, menuItems }, ref) => {
+    return (
+      <div ref={ref} className={styles.overflowMenuWrapper} style={position}>
+        <ul className={styles.menuList}>
+          {menuItems?.map(item => (
+            <li key={item.name} className={styles.itemWrapper} onClick={item.onClick}>
+              <span className={styles.icon}>{item.icon}</span>
+              <span className={styles.name}>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+);
+
+export default OverflowMenu;
