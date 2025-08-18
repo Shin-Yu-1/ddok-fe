20efbb88893897265534cd286190e7a53397259f
Shin-Yu-1 <shingyu.user@gmail.com>
2025-08-18 19:28:08 +0900

    feat: delete api hook 추가 / DP-236

---
+++ src/hooks/useDeleteApi.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+import { api } from '@/api/api';
+
+interface DeleteApiOptions {
+  url: string;
+  params?: Record<string, unknown>;
+}
+
+/**
+ * 사용 예:
+ * const deleteUser = useDeleteApi<RESPONSE_TYPE>({ url: '/DELETE_URL' });
+ *
+ * 요청: deleteUser.mutate();
+ * 요청 및 응답:
+ * deleteUser.mutate(undefined, {
+ *   onSuccess: SUCCESS_FUNCTION,
+ *   onError: ERROR_FUNCTION
+ * });
+ */
+export const useDeleteApi = <TResponse>({ url, params }: DeleteApiOptions) => {
+  return useMutation<TResponse>({
+    mutationFn: async () => {
+      const { data } = await api.delete<TResponse>(url, { params });
+      return data;
+    },
+  });
+};
