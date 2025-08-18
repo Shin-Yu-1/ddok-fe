64a64592dcd84817da23f3f6547be5662cfd8e63
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-08-18 09:41:57 +0900

    feat: router 설정 / DP-234

---
+++ src/router/router.tsx
@@ -0,0 +1,169 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from '@/App';
+import AuthLayout from '@/layouts/AuthLayout/AuthLayout';
+import MapLayout from '@/layouts/MapLayout/MapLayout';
+import PersonalizationLayout from '@/layouts/PersonalizationLayout/PersonalizationLayout';
+import PostLayout from '@/layouts/PostLayout/PostLayout';
+import ProfileLayout from '@/layouts/ProfileLayout/ProfileLayout';
+import SearchLayout from '@/layouts/SearchLayout/SearchLayout';
+import TeamLayout from '@/layouts/TeamLayout/TeamLayout';
+import FindIdPage from '@/pages/auth/FindIdPage/FindIdPage';
+import FindPasswordPage from '@/pages/auth/FindPasswordPage/FindPasswordPage';
+import ResetPasswordPage from '@/pages/auth/ResetPasswordPage/ResetPasswordPage';
+import SignInPage from '@/pages/auth/SignInPage/SignInPage';
+import SignUpPage from '@/pages/auth/SignUpPage/SignUpPage';
+import CreateProjectPage from '@/pages/create/CreateProjectPage/CreateProjectPage';
+import CreateStudyPage from '@/pages/create/CreateStudyPage/CreateStudyPage';
+import DetailProjectPage from '@/pages/detail/DetailProjectPage/DetailProjectPage';
+import DetailStudyPage from '@/pages/detail/DetailStudyPage/DetailStudyPage';
+import EditMyInfoPage from '@/pages/edit/EditMyInfoPage/EditMyInfoPage';
+import EditProjectPage from '@/pages/edit/EditProjectPage/EditProjectPage';
+import EditStudyPage from '@/pages/edit/EditStudyPage/EditStudyPage';
+import IntroPage from '@/pages/intro/IntroPage/IntroPage';
+import MapPage from '@/pages/map/MapPage/MapPage';
+import PersonalizationPage from '@/pages/personalization/PersonalizationPage/PersonalizationPage';
+import MyProfilePage from '@/pages/profile/MyProfilePage/MyProfilePage';
+import UserProfilePage from '@/pages/profile/UserProfilePage/UserProfilePage';
+import RankingPage from '@/pages/ranking/RankingPage/RankingPage';
+import SearchPlayerPage from '@/pages/search/SearchPlayerPage/SearchPlayerPage';
+import SearchProjectPage from '@/pages/search/SearchProjectPage/SearchProjectPage';
+import SearchStudyPage from '@/pages/search/SearchStudyPage/SearchStudyPage';
+import TeamPage from '@/pages/team/TeamPage/TeamPage';
+import TeamSettingPage from '@/pages/team/TeamSettingPage/TeamSettingPage';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // 글로벌 컴포넌트용
+    children: [
+      {
+        index: true,
+        element: <IntroPage />,
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: 'auth/signin',
+            element: <SignInPage />,
+          },
+          {
+            path: 'auth/SignUp',
+            element: <SignUpPage />,
+          },
+          {
+            path: 'auth/FindId',
+            element: <FindIdPage />,
+          },
+          {
+            path: 'auth/FindPassword',
+            element: <FindPasswordPage />,
+          },
+          {
+            path: 'auth/ResetPassword',
+            element: <ResetPasswordPage />,
+          },
+        ],
+      },
+      {
+        element: <PersonalizationLayout />,
+        children: [
+          {
+            path: 'personalization',
+            element: <PersonalizationPage />,
+          },
+        ],
+      },
+      {
+        element: <MapLayout />,
+        children: [
+          {
+            path: 'map',
+            element: <MapPage />,
+          },
+        ],
+      },
+      {
+        element: <SearchLayout />,
+        children: [
+          {
+            path: 'search/project',
+            element: <SearchProjectPage />,
+          },
+          {
+            path: 'search/study',
+            element: <SearchStudyPage />,
+          },
+          {
+            path: 'search/player',
+            element: <SearchPlayerPage />,
+          },
+        ],
+      },
+      {
+        element: <PostLayout />,
+        children: [
+          {
+            path: 'create/project',
+            element: <CreateProjectPage />,
+          },
+          {
+            path: 'edit/project/:id',
+            element: <EditProjectPage />,
+          },
+          {
+            path: 'create/study',
+            element: <CreateStudyPage />,
+          },
+          {
+            path: 'edit/study/:id',
+            element: <EditStudyPage />,
+          },
+          {
+            path: 'detail/project/:id',
+            element: <DetailProjectPage />,
+          },
+          {
+            path: 'detail/study/:id',
+            element: <DetailStudyPage />,
+          },
+        ],
+      },
+      {
+        element: <ProfileLayout />,
+        children: [
+          {
+            path: 'profile/user/:id',
+            element: <UserProfilePage />,
+          },
+          {
+            path: 'profile/my',
+            element: <MyProfilePage />,
+          },
+          {
+            path: 'profile/my/edit',
+            element: <EditMyInfoPage />,
+          },
+        ],
+      },
+      {
+        element: <TeamLayout />,
+        children: [
+          {
+            path: 'team/:id',
+            element: <TeamPage />,
+          },
+          {
+            path: 'team/:id/setting',
+            element: <TeamSettingPage />,
+          },
+        ],
+      },
+      {
+        path: 'ranking',
+        element: <RankingPage />,
+      },
+    ],
+  },
+]);
