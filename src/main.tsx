08b7d747445b1e8a1a7e50744cc32711d2e128a0
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-08-18 09:42:19 +0900

    feat: 라우터 적용 / DP-234

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import { router } from './router/router';
 import './styles/index.scss';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
