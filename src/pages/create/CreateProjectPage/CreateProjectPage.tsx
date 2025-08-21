c42b6354161a3a076459098316198d8d36780bf3
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-08-22 08:48:23 +0900

    feat(CreateProjectPage): 섹션 기초 세팅 진행 / DP-262

--- src/pages/create/CreateProjectPage/CreateProjectPage.tsx
+++ src/pages/create/CreateProjectPage/CreateProjectPage.tsx
@@ -1,6 +1,9 @@
+import { MagicWand } from '@phosphor-icons/react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 import Button from '@/components/Button/Button';
+import MainSection from '@/components/PostPagesSection/MainSection/MainSection';
+import SideSection from '@/components/PostPagesSection/SideSection/SideSection';
 
 import styles from './CreateProjectPage.module.scss';
 
@@ -23,10 +26,44 @@ const CreateProjectPage = () => {
                 모집 공고 등록하기
               </Button>
             </div>
-            <div className={styles.nameSection}>팀명 컴포넌트 들어갈 예정</div>
+            <div className={styles.nameSection}>
+              <MainSection title={'프로젝트 제목'}>추후 input 바 들어갈 예정</MainSection>
+            </div>
             <div className={styles.detailInfoSection}>
-              <div className={styles.leftSection}>왼쪽</div>
-              <div className={styles.rightSection}>오른쪽</div>
+              <div className={styles.leftSection}>
+                <MainSection title={'모집 현황'}>모집 현황 테이블 들어갈 예정</MainSection>
+                <MainSection title={'이런 분을 찾습니다!'}>
+                  성향 입력 컴포넌트 들어갈 예정
+                </MainSection>
+                <MainSection
+                  title={'모집 공고 상세'}
+                  titleAction={
+                    <>
+                      <button className={styles.useAIWrite}>
+                        <MagicWand
+                          size={25}
+                          weight="light"
+                          color="var(--white-3)"
+                          className="aiIcon"
+                        ></MagicWand>
+                      </button>
+                    </>
+                  }
+                >
+                  마크다운 작성 양식 들어갈 예정
+                </MainSection>
+              </div>
+              <div className={styles.rightSections}>
+                <SideSection readonly={true} title={'진행 상태'}>
+                  진행 상태 버튼 들어갈 예정
+                </SideSection>
+                <SideSection title={'모집 인원'}>모집 인원 드롭 추가 예정</SideSection>
+                <SideSection title={'시작 예정일'}>날짜 선택 섹션 추가 예정</SideSection>
+                <SideSection title={'예상 기간'}>날짜 선택 섹션 추가 예정</SideSection>
+                <MainSection title={'모임 형태'}>모임 형태 선택 섹션</MainSection>
+                <SideSection title={'지역'}>지역 선택 기능 추가 예정</SideSection>
+                <MainSection title={'희망 나이대'}>희망 나이대 선택 섹션</MainSection>
+              </div>
             </div>
           </div>
         </div>
