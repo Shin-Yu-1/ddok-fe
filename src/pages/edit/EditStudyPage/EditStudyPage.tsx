cee79ce66c14a977fa80a7713aa3924b47de1e36
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-08-21 15:27:02 +0900

    feat: 동일 구조 적용 / DP-244

--- src/pages/edit/EditStudyPage/EditStudyPage.tsx
+++ src/pages/edit/EditStudyPage/EditStudyPage.tsx
@@ -2,9 +2,22 @@ import styles from './EditStudyPage.module.scss';
 
 const EditStudyPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <h1 className={styles.title}>EditStudyPage</h1>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.bannerImage}>배너 이미지 섹션</div>
+        <div className={styles.postContainer}>
+          <div className={styles.postContentsLayout}>
+            <div className={styles.actionsLine}>버튼 section 컴포넌트 들어갈 예정</div>
+            <div className={styles.nameSection}>팀명 컴포넌트 들어갈 예정</div>
+            <div className={styles.detailInfoSection}>
+              <div className={styles.leftSection}>왼쪽</div>
+              <div className={styles.rightSections}>오른쪽</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
