d52f1b7497fe4d0672f6663b116abbe5df74e2b5
eunlee <eklee2057@naver.com>
2025-08-22 11:38:44 +0900

    feat: 서브 패널 title 추가 / DP-255

--- src/features/map/components/MapSubPanel/MapSubPanel.tsx
+++ src/features/map/components/MapSubPanel/MapSubPanel.tsx
@@ -7,8 +7,13 @@ interface MapSubPanelProps {
 const MapSubPanel: React.FC<MapSubPanelProps> = ({ isOpen }) => {
   return (
     <div className={styles.subPanel__container}>
+      {/* 패널 타이틀*/}
+      <div className={styles.panel__title}>숨참고 딥다이브 카페</div>
+
       {/* 패널 상태 표시 (임시) */}
-      <div className={styles.subPanel__openStatus}>패널 상태 : {isOpen ? 'OPENED' : 'CLOSED'}</div>
+      <div className={styles.subPanel__openStatus}>
+        서브 패널 상태 : {isOpen ? 'OPENED' : 'CLOSED'}
+      </div>
     </div>
   );
 };
