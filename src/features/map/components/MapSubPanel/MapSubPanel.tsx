7f41c2c742353a8c12b4351444fb2441cda8e39c
eunlee <eklee2057@naver.com>
2025-08-22 12:00:31 +0900

    chore: 불필요한 코드 및 주석 제거 / DP-255

--- src/features/map/components/MapSubPanel/MapSubPanel.tsx
+++ src/features/map/components/MapSubPanel/MapSubPanel.tsx
@@ -1,19 +1,10 @@
 import styles from './MapSubPanel.module.scss';
 
-interface MapSubPanelProps {
-  isOpen: boolean;
-}
-
-const MapSubPanel: React.FC<MapSubPanelProps> = ({ isOpen }) => {
+const MapSubPanel = () => {
   return (
     <div className={styles.subPanel__container}>
-      {/* 패널 타이틀*/}
+      {/* 서브 패널 타이틀*/}
       <div className={styles.panel__title}>숨참고 딥다이브 카페</div>
-
-      {/* 패널 상태 표시 (임시) */}
-      <div className={styles.subPanel__openStatus}>
-        서브 패널 상태 : {isOpen ? 'OPENED' : 'CLOSED'}
-      </div>
     </div>
   );
 };
