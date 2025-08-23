e311dc061c82748d7145ff9e03ea87f57c26c778
ssoogit <150372021+ssoogit@users.noreply.github.com>
2025-08-24 07:17:44 +0900

    feat(subHeader): 현재 경로 기반 활성 상태 설정 / DP-247

--- src/features/Header/components/SubHeader.tsx
+++ src/features/Header/components/SubHeader.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 
 import Button from '@/components/Button/Button';
 
+import { useActiveRoute } from '../hooks/useActiveRoute';
 import { useSubHeaderHandlers } from '../hooks/useSubHeaderHandlers';
 
 import styles from './SubHeader.module.scss';
@@ -14,40 +15,46 @@ interface SubHeaderProps {
 
 const SubHeader = forwardRef<HTMLElement, SubHeaderProps>(({ className }, ref) => {
   const handlers = useSubHeaderHandlers();
+  const activeStates = useActiveRoute();
 
   const navItems = [
     {
       label: '지도',
+      isActive: activeStates.map,
       onclick: handlers.handleMapClick,
     },
     {
       label: '프로젝트',
+      isActive: activeStates.project,
       onclick: handlers.handleProjectClick,
     },
     {
       label: '스터디',
+      isActive: activeStates.study,
       onclick: handlers.handleStudyClick,
     },
     {
       label: '플레이어',
+      isActive: activeStates.player,
       onclick: handlers.handlePlayerClick,
     },
     {
       label: '랭킹',
+      isActive: activeStates.ranking,
       onclick: handlers.handleRankingClick,
     },
   ];
   return (
     <nav ref={ref} className={clsx(styles.subHeader, className)} role="navigation">
       <div className={styles.navContainer}>
-        {navItems.map(({ label, onclick }) => (
+        {navItems.map(({ label, onclick, isActive }) => (
           <Button
             key={label}
             variant="outline"
             size="sm"
             fontSizePreset="xsmall"
             fontWeightPreset="regular"
-            className={styles.navButton}
+            className={clsx(styles.navButton, { [styles.active]: isActive })}
             onClick={onclick}
           >
             {label}
