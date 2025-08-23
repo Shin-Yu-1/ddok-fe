39bc88656bbad323e8101132d33162332a26ae84
ssoogit <150372021+ssoogit@users.noreply.github.com>
2025-08-24 06:53:33 +0900

    feat(subHeader): 네비게이션 핸들러 제작 및 연결 / DP-247

--- src/features/Header/components/SubHeader.tsx
+++ src/features/Header/components/SubHeader.tsx
@@ -4,6 +4,8 @@ import clsx from 'clsx';
 
 import Button from '@/components/Button/Button';
 
+import { useSubHeaderHandlers } from '../hooks/useSubHeaderHandlers';
+
 import styles from './SubHeader.module.scss';
 
 interface SubHeaderProps {
@@ -11,27 +13,34 @@ interface SubHeaderProps {
 }
 
 const SubHeader = forwardRef<HTMLElement, SubHeaderProps>(({ className }, ref) => {
+  const handlers = useSubHeaderHandlers();
+
   const navItems = [
     {
       label: '지도',
+      onclick: handlers.handleMapClick,
     },
     {
       label: '프로젝트',
+      onclick: handlers.handleProjectClick,
     },
     {
       label: '스터디',
+      onclick: handlers.handleStudyClick,
     },
     {
       label: '플레이어',
+      onclick: handlers.handlePlayerClick,
     },
     {
       label: '랭킹',
+      onclick: handlers.handleRankingClick,
     },
   ];
   return (
     <nav ref={ref} className={clsx(styles.subHeader, className)} role="navigation">
       <div className={styles.navContainer}>
-        {navItems.map(({ label }) => (
+        {navItems.map(({ label, onclick }) => (
           <Button
             key={label}
             variant="outline"
@@ -39,6 +48,7 @@ const SubHeader = forwardRef<HTMLElement, SubHeaderProps>(({ className }, ref) =
             fontSizePreset="xsmall"
             fontWeightPreset="regular"
             className={styles.navButton}
+            onClick={onclick}
           >
             {label}
           </Button>
