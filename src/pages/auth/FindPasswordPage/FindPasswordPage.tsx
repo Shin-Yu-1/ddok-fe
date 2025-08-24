d8adc622071966eb7a61a90f97e2dec499c57ad0
projectmiluju <dnjsdyd0712@naver.com>
2025-08-25 05:25:06 +0900

    feat(FindPasswordPage): 비밀번호 찾기 페이지 스타일 및 구조 개선 / DP-257

--- src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
+++ src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
@@ -1,11 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import FindPasswordForm from '@/features/Auth/FindPasswordForm/FIndPasswordForm';
+
 import styles from './FindPasswordPage.module.scss';
 
-const FindPasswordPage = () => {
+export default function FindPasswordPage() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>FindPasswordPage</h1>
+    <div className={styles.inner}>
+      <h1 className={styles.title}>비밀번호를 잊어버리셨습니까?</h1>
+      <p className={styles.text}>
+        가입한 이름과 이메일, 전화번호 정보를 입력하고, 새로운 비밀번호로 변경하세요!
+      </p>
+
+      <FindPasswordForm />
+
+      <div className={styles.links}>
+        <Link to="/find-id" className={styles.link}>
+          이메일이 기억이 나지 않습니다..
+        </Link>
+        <Link to="/sign-in" className={`${styles.link} ${styles.linkWarning}`}>
+          <span>비밀번호가 기억 나는거 같아요!</span>
+        </Link>
+      </div>
     </div>
   );
-};
-
-export default FindPasswordPage;
+}
