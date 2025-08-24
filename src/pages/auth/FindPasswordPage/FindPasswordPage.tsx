0b3c79a06bfe8bb845571805c44f066fc320246e
projectmiluju <dnjsdyd0712@naver.com>
2025-08-25 07:04:09 +0900

    fix(FindPasswordPage): 수정된 비밀번호 입력 안내 문구 및 잘못된 경로 수정 / DP-257

--- src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
+++ src/pages/auth/FindPasswordPage/FindPasswordPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import FindPasswordForm from '@/features/Auth/FindPasswordForm/FIndPasswordForm';
+import FindPasswordForm from '@/features/Auth/FindPasswordForm/FindPasswordForm';
 
 import styles from './FindPasswordPage.module.scss';
 
@@ -8,9 +8,7 @@ export default function FindPasswordPage() {
   return (
     <div className={styles.inner}>
       <h1 className={styles.title}>비밀번호를 잊어버리셨습니까?</h1>
-      <p className={styles.text}>
-        가입한 이름과 이메일, 전화번호 정보를 입력하고, 새로운 비밀번호로 변경하세요!
-      </p>
+      <p className={styles.text}>가입한 정보를 입력하고, 새로운 비밀번호로 변경하세요!</p>
 
       <FindPasswordForm />
 
