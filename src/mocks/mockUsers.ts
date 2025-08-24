3f21c19a6e0192a416b493211fce5f8d23351d3c
projectmiluju <dnjsdyd0712@naver.com>
2025-08-25 02:49:50 +0900

    chore(mocks): mockAuth 및 mockUsers 파일 추가 / DP-257

---
+++ src/mocks/mockUsers.ts
@@ -0,0 +1,21 @@
+export interface MockUser {
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  nickname: string;
+}
+
+export const mockUsers: MockUser[] = [
+  {
+    id: 1,
+    email: 'test@test.com',
+    password: '123456',
+    name: '테스트',
+    nickname: '테스트 사용자',
+  },
+];
+
+export const findUserByCredentials = (email: string, password: string): MockUser | undefined => {
+  return mockUsers.find(user => user.email === email && user.password === password);
+};
