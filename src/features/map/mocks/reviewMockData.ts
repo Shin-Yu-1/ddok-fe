077fb9d4265ab93d4fcd8dedca77db3e2762679b
eunlee <eklee2057@naver.com>
2025-08-25 14:08:30 +0900

    feat: 후기 데이터 추가 / DP-263

--- src/features/map/mocks/reviewMockData.ts
+++ src/features/map/mocks/reviewMockData.ts
@@ -10,6 +10,14 @@ export const reviewMockData: CafeReview[] = [
         tagName: '분위기가 좋아요',
         tagCount: 32,
       },
+      {
+        tagName: '음료가 맛있어요',
+        tagCount: 25,
+      },
+      {
+        tagName: '직원이 친절해요',
+        tagCount: 18,
+      },
     ],
     totalRating: 3.9,
   },
