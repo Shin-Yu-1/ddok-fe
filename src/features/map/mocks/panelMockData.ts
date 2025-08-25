14f50f05f9dca9f5e980ac9988fee2c0f09a6428
eunlee <eklee2057@naver.com>
2025-08-25 18:43:48 +0900

    fix: 좌표 변경 / DP-263

--- src/features/map/mocks/panelMockData.ts
+++ src/features/map/mocks/panelMockData.ts
@@ -12,7 +12,7 @@ export const panelMockData: MapItem[] = [
     projectId: 1,
     title: '구지라지 프로젝트',
     location: {
-      latitude: 37.5665,
+      latitude: 37.567,
       longitude: 126.978,
       address: '서울특별시 강남구 테헤란로',
     },
@@ -24,8 +24,8 @@ export const panelMockData: MapItem[] = [
     projectId: 2,
     title: '똑똑 프로젝트',
     location: {
-      latitude: 37.5665,
-      longitude: 126.978,
+      latitude: 37.566,
+      longitude: 126.977,
       address: '서울특별시 강남구 테헤란로',
     },
     teamStatus: 'ONGOING',
@@ -38,7 +38,7 @@ export const panelMockData: MapItem[] = [
     studyId: 1,
     title: '구지라지 스터디',
     location: {
-      latitude: 37.5665,
+      latitude: 37.565,
       longitude: 126.978,
       address: '서울특별시 강남구 테헤란로',
     },
@@ -50,8 +50,8 @@ export const panelMockData: MapItem[] = [
     studyId: 2,
     title: '똑똑 스터디',
     location: {
-      latitude: 37.5665,
-      longitude: 126.978,
+      latitude: 37.566,
+      longitude: 126.98,
       address: '서울특별시 강남구 테헤란로',
     },
     teamStatus: 'RECRUITING',
@@ -64,8 +64,8 @@ export const panelMockData: MapItem[] = [
     userId: 1,
     nickname: '똑똑한 백엔드',
     location: {
-      latitude: 37.5665,
-      longitude: 126.978,
+      latitude: 37.565,
+      longitude: 126.977,
       address: '서울특별시 강남구 테헤란로',
     },
     profileImageUrl: '/src/assets/images/avatar.png',
@@ -77,8 +77,8 @@ export const panelMockData: MapItem[] = [
     cafeId: 1,
     title: '구지라지 카페',
     location: {
-      latitude: 37.5665,
-      longitude: 126.978,
+      latitude: 37.564,
+      longitude: 126.976,
       address: '서울특별시 강남구 테헤란로',
     },
     bannerImageUrl: '/src/assets/images/avatar.png',
@@ -88,8 +88,8 @@ export const panelMockData: MapItem[] = [
     cafeId: 2,
     title: '새로운 카페',
     location: {
-      latitude: 37.5665,
-      longitude: 126.978,
+      latitude: 37.563,
+      longitude: 126.977,
       address: '서울특별시 강남구 테헤란로',
     },
     bannerImageUrl: '/src/assets/images/avatar.png',
