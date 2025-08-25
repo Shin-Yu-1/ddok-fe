02c420ec7ad781e464e67672331729e64cb2e29d
eunlee <eklee2057@naver.com>
2025-08-25 12:52:50 +0900

    feat: 카페 리뷰 타입 추가 / DP-263

--- src/features/map/types/cafe.ts
+++ src/features/map/types/cafe.ts
@@ -4,6 +4,19 @@ import type { BaseMapItem } from './common';
  * 카페 관련 타입 정의
  */
 
+export interface CafeReview {
+  cafeId: number;
+  title: string;
+  reviewCount: number;
+  cafeReviewTag: [
+    {
+      tagName: string;
+      tagCount: number;
+    },
+  ];
+  totalRating: number;
+}
+
 export interface Cafe extends BaseMapItem {
   category: 'cafe';
   cafeId: number;
