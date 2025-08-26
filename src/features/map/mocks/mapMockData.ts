0a5708f6731cbaa998e6933b00c11164c142adcd
eunlee <eklee2057@naver.com>
2025-08-26 12:08:53 +0900

    refactor: 지도 목 데이터 분리 / DP-263

---
+++ src/features/map/mocks/mapMockData.ts
@@ -0,0 +1,68 @@
+export const mapMockData = [
+  {
+    category: 'project',
+    projectId: 1,
+    title: '구지라지 프로젝트',
+    location: {
+      latitude: 37.567,
+      longitude: 126.978,
+      address: '서울특별시 강남구 테헤란로…',
+    },
+    teamStatus: 'RECRUITING',
+  },
+  {
+    category: 'project',
+    projectId: 2,
+    title: '똑똑 프로젝트',
+    location: {
+      latitude: 37.566,
+      longitude: 126.977,
+      address: '서울특별시 강남구 테헤란로',
+    },
+    teamStatus: 'ONGOING',
+  },
+  {
+    category: 'study',
+    studyId: 1,
+    title: '구지라지 스터디',
+    location: {
+      latitude: 37.565,
+      longitude: 126.978,
+      address: '서울특별시 강남구 테헤란로…',
+    },
+    teamStatus: 'ONGOING',
+  },
+  {
+    category: 'study',
+    studyId: 2,
+    title: '똑똑 스터디',
+    location: {
+      latitude: 37.566,
+      longitude: 126.98,
+      address: '서울특별시 강남구 테헤란로',
+    },
+    teamStatus: 'ONGOING',
+  },
+  {
+    category: 'player',
+    userId: 1,
+    nickname: '똑똑한 백엔드',
+    location: {
+      latitude: 37.565,
+      longitude: 126.977,
+      address: '서울특별시 강남구 테헤란로…',
+    },
+    position: '백엔드',
+    isMine: false,
+  },
+  {
+    category: 'cafe',
+    cafeId: 1,
+    title: '구지라지 카페',
+    location: {
+      latitude: 37.564,
+      longitude: 126.976,
+      address: '서울특별시 강남구 테헤란로…',
+    },
+  },
+];
