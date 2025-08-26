49624ee9ebe1ff3bd649b1e4ed7674b7d6ff6174
eunlee <eklee2057@naver.com>
2025-08-26 12:09:19 +0900

    refactor: 지도 목 데이터 분리 / DP-263

--- src/pages/map/MapPage/MapPage.tsx
+++ src/pages/map/MapPage/MapPage.tsx
@@ -6,77 +6,9 @@ import Button from '@/components/Button/Button';
 import MapOverlay from '@/features/map/components/MapOverlay/MapOverlay';
 import MapPanel from '@/features/map/components/MapPanel/MapPanel';
 import MapSubPanel from '@/features/map/components/MapSubPanel/MapSubPanel';
+import { mapMockData } from '@/features/map/mocks/mapMockData';
 
 import styles from './MapPage.module.scss';
-
-const mapMockData = [
-  {
-    category: 'project',
-    projectId: 1,
-    title: '구지라지 프로젝트',
-    location: {
-      latitude: 37.567,
-      longitude: 126.978,
-      address: '서울특별시 강남구 테헤란로…',
-    },
-    teamStatus: 'RECRUITING',
-  },
-  {
-    category: 'project',
-    projectId: 2,
-    title: '똑똑 프로젝트',
-    location: {
-      latitude: 37.566,
-      longitude: 126.977,
-      address: '서울특별시 강남구 테헤란로',
-    },
-    teamStatus: 'ONGOING',
-  },
-  {
-    category: 'study',
-    studyId: 1,
-    title: '구지라지 스터디',
-    location: {
-      latitude: 37.565,
-      longitude: 126.978,
-      address: '서울특별시 강남구 테헤란로…',
-    },
-    teamStatus: 'ONGOING',
-  },
-  {
-    category: 'study',
-    studyId: 2,
-    title: '똑똑 스터디',
-    location: {
-      latitude: 37.566,
-      longitude: 126.98,
-      address: '서울특별시 강남구 테헤란로',
-    },
-    teamStatus: 'ONGOING',
-  },
-  {
-    category: 'player',
-    userId: 1,
-    nickname: '똑똑한 백엔드',
-    location: {
-      latitude: 37.565,
-      longitude: 126.977,
-      address: '서울특별시 강남구 테헤란로…',
-    },
-    position: '백엔드',
-    isMine: false,
-  },
-  {
-    category: 'cafe',
-    cafeId: 1,
-    title: '구지라지 카페',
-    location: {
-      latitude: 37.564,
-      longitude: 126.976,
-      address: '서울특별시 강남구 테헤란로…',
-    },
-  },
-];
 
 const MapPage = () => {
   const [isOverlayOpen, setIsOverlayOpen] = useState(false);
@@ -167,7 +99,7 @@ const MapPage = () => {
               <CustomOverlayMap position={selectedPoint} yAnchor={1.13}>
                 <MapOverlay
                   onOverlayClose={() => setIsOverlayOpen(false)}
-                  overlayType={selectedPoint.type}
+                  overlayType={selectedPoint.type as 'project' | 'study' | 'player' | 'cafe'}
                 />
               </CustomOverlayMap>
             )}
