df2077160390f26a8f2977cb041137133fc321e6
projectmiluju <dnjsdyd0712@naver.com>
2025-08-26 14:10:30 +0900

    feat(AuthLayout): 헤더 추가 및 스타일 적용 / DP-257

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/features/Header/components/Header';
+
 import styles from './AuthLayout.module.scss';
 
 const AuthLayout = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.header}>
+        <Header variant="logo-only" />
+      </div>
       <main className={styles.content}>
         <Outlet />
       </main>
