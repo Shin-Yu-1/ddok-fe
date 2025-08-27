de479ab56d2aa1c5c86cdd17cb1aee819bb884ad
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-08-27 21:48:42 +0900

    feat(PostLayout): 헤더, 서브헤더, 사이드바 적용 / DP-262

--- src/layouts/PostLayout/PostLayout.tsx
+++ src/layouts/PostLayout/PostLayout.tsx
@@ -1,12 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/features/Header/components/Header';
+import SubHeader from '@/features/Header/components/SubHeader';
+import Sidebar from '@/features/Sidebar/components/Sidebar';
+
 import styles from './PostLayout.module.scss';
 
 const PostLayout = () => {
   return (
-    <div className={styles.container}>
-      <Outlet />
-    </div>
+    <>
+      <div className={styles.layoutContainer}>
+        <Header />
+        <SubHeader />
+        <Sidebar />
+        <div className={styles.contentContainer}>
+          <Outlet />
+        </div>
+      </div>
+    </>
   );
 };
 
