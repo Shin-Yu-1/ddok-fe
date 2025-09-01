cac378cad71943179d7f568cc7d043e95fd97dda
Shin-Yu-1 <shingyu.user@gmail.com>
2025-09-01 17:59:16 +0900

    fix(layout): 검색 레이아웃에 헤더, 서브헤더, 사이드바 추가 및 스타일링 수정 / DP-304

--- src/layouts/SearchLayout/SearchLayout.tsx
+++ src/layouts/SearchLayout/SearchLayout.tsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/features/Header/components/Header';
+import SubHeader from '@/features/Header/components/SubHeader';
+import Sidebar from '@/features/Sidebar/components/Sidebar';
+
 import styles from './SearchLayout.module.scss';
 
 const SearchLayout = () => {
   return (
-    <div className={styles.container}>
-      <Outlet />
+    <div className={styles.layoutContainer}>
+      <Header variant="user" />
+      <SubHeader />
+      <Sidebar />
+      <div className={styles.contentContainer}>
+        <Outlet />
+      </div>
     </div>
   );
 };
