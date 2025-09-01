b21ff289e3c4b0e105e12fc86cb554a08a191983
Shin-Yu-1 <shingyu.user@gmail.com>
2025-09-01 13:34:41 +0900

    feat(schema): 플레이어 조회 리스트 스키마 추가 / DP-304

---
+++ src/schemas/player.schema.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+import { apiResponseSchema } from '@/schemas/api.schema';
+
+import { paginationSchema } from './chat.schema';
+
+export const playerSchema = z.object({
+  userId: z.number(),
+  category: z.string(),
+  nickname: z.string(),
+  profileImageUrl: z.string(),
+  mainBagde: {
+    type: z.string(),
+    tier: z.string(),
+  },
+  abandonBagde: {
+    isGranted: z.boolean(),
+    count: z.number(),
+  },
+  mainPosition: z.string(),
+  address: z.string(),
+  temperature: z.number(),
+  isMine: z.boolean(),
+  chatRoomId: z.number(),
+  dmRequestPending: z.boolean(),
+});
+export type Player = z.infer<typeof playerSchema>;
+
+export const playerListSchema = z.object({
+  items: z.array(playerSchema),
+  pagination: paginationSchema,
+});
+
+export const playerListApiResponseSchema = apiResponseSchema(playerListSchema);
+export type ChatListApiResponse = z.infer<typeof playerListApiResponseSchema>;
