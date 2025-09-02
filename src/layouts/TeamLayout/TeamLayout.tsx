2b30814c41627a588dabefa42f42d2d458a0ed32
eunlee <eklee2057@naver.com>
2025-09-02 23:33:14 +0900

    feat: 팀 페이지 레이아웃 설정 / DP-313

--- src/layouts/TeamLayout/TeamLayout.tsx
+++ src/layouts/TeamLayout/TeamLayout.tsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom';
 
+import Header from '@/features/Header/components/Header';
+import SubHeader from '@/features/Header/components/SubHeader';
+import Sidebar from '@/features/Sidebar/components/Sidebar';
+
 import styles from './TeamLayout.module.scss';
 
 const TeamLayout = () => {
   return (
-    <div className={styles.container}>
-      <Outlet />
+    <div className={styles.layoutContainer}>
+      <Header />
+      <SubHeader />
+      <Sidebar />
+      <div className={styles.contentContainer}>
+        <Outlet />
+      </div>
     </div>
   );
 };
