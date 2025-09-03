11bc2fb937254e6dd77c34079607ac7403ddbcf3
projectmiluju <dnjsdyd0712@naver.com>
2025-09-03 14:15:49 +0900

    refactor(SearchLayout): import 문 정리 및 중복 제거 / DP-304

--- src/layouts/SearchLayout/SearchLayout.tsx
+++ src/layouts/SearchLayout/SearchLayout.tsx
@@ -1,24 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
+import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute';
 import Header from '@/features/Header/components/Header';
 import SubHeader from '@/features/Header/components/SubHeader';
 import Sidebar from '@/features/Sidebar/components/Sidebar';
-        
-import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute';
 
 import styles from './SearchLayout.module.scss';
 
 const SearchLayout = () => {
   return (
     <ProtectedRoute requirePreference={true}>
-    <div className={styles.layoutContainer}>
-      <Header variant="user" />
-      <SubHeader />
-      <Sidebar />
-      <div className={styles.contentContainer}>
-        <Outlet />
+      <div className={styles.layoutContainer}>
+        <Header variant="user" />
+        <SubHeader />
+        <Sidebar />
+        <div className={styles.contentContainer}>
+          <Outlet />
+        </div>
       </div>
-    </div>
     </ProtectedRoute>
   );
 };
