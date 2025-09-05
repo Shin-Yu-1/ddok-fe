3adaed91f0bb910af814da75df9b917adee10dd4
eunlee <eklee2057@naver.com>
2025-09-05 22:39:56 +0900

    fix: 컴포넌트명 변경 / DP-334

--- src/features/Team/components/UserRow/UserRow.tsx
+++ src/features/Team/components/UserRow/UserRow.tsx
@@ -1,6 +1,12 @@
-import styles from './User.module.scss';
+import type { UserType } from '../../schemas/teamMemberSchema';
 
-const User = () => {
+import styles from './UserRow.module.scss';
+
+export interface UserProps {
+  user: UserType;
+}
+
+const User = ({ user }: UserProps) => {
   return (
     <div className={styles.user}>
       <div className={styles.user__item}>
@@ -10,12 +16,12 @@ const User = () => {
             src="/src/assets/images/avatar.png"
             alt="Banner"
           />
-          <div className={styles.user__item__left__nickname}>용</div>
+          <div className={styles.user__item__left__nickname}>{user.nickname}</div>
           <div className={styles.user__item__left__badges}>배지 리스트 표시</div>
         </div>
         <div className={styles.user__item__right}>
-          <div className={styles.user__item__right__position}>풀스택</div>
-          <div className={styles.user__item__right__temperature}>36.5℃</div>
+          <div className={styles.user__item__right__position}>{user.mainPosition}</div>
+          <div className={styles.user__item__right__temperature}>{user.temperature}℃</div>
         </div>
       </div>
     </div>
