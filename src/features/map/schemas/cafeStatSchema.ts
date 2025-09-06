96c70679f0e3c36e9e0102a1325349e96e47615a
eunlee <eklee2057@naver.com>
2025-09-06 21:56:55 +0900

    feat: 카페 후기 통계와 관련된 스키마 및 타입 추가 / DP-318

---
+++ src/features/map/schemas/cafeStatSchema.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod';
+
+/**
+ * 리뷰 내 평가 태그
+ */
+export const CafeReviewTagSchema = z.object({
+  tagName: z.string(),
+  tagCount: z.number(),
+});
+
+/**
+ * 리뷰 통계 정보
+ */
+export const CafeStatSchema = z.object({
+  cafeId: z.number(),
+  title: z.string(),
+  reviewCount: z.number(),
+  cafeReviewTag: z.array(CafeReviewTagSchema),
+  totalRating: z.number(),
+});
+
+/**
+ * 리뷰 통계 조회 API 응답 스키마
+ */
+export const CafeStatResponseSchema = z.object({
+  data: CafeStatSchema,
+  message: z.string(),
+  status: z.number(),
+});
+
+/**
+ * 리뷰 내 평가 태그 타입
+ */
+export type CafeReview = z.infer<typeof CafeReviewTagSchema>;
+
+/**
+ * 리뷰 통계 정보 타입
+ */
+export type CafeStat = z.infer<typeof CafeStatSchema>;
+
+/**
+ * 리뷰 통계 조회 API 응답 타입
+ */
+export type CafeStatResponse = z.infer<typeof CafeStatResponseSchema>;
