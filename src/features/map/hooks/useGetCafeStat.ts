53c9f3fb1c7586e599d19ff3380da4fcc35e020a
eunlee <eklee2057@naver.com>
2025-09-06 22:29:18 +0900

    feat: 카페 통계 정보를 가져오는 리액트 쿼리 추가 / DP-318

--- src/features/map/hooks/useGetCafeStat.ts
+++ src/features/map/hooks/useGetCafeStat.ts
@@ -11,8 +11,8 @@ interface UseGetCafeStatOptions {
  * 카페 통계 정보를 가져오는 리액트 쿼리 hook
  *
  * @param cafeId - 조회할 카페 ID
- * @param enabled - 쿼리 실행 여부 (기본값: true)
- * @returns 카페 통계 정보 쿼리 결과
+ * @param enabled - 쿼리 실행 여부
+ * @returns 카페 통계 정보 조회 쿼리 결과
  */
 export const useGetCafeStat = ({ cafeId, enabled = true }: UseGetCafeStatOptions) => {
   return useGetApi<CafeStatResponse>({
