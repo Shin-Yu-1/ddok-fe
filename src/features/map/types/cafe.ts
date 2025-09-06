0f2b5e6eca0fce2e62439cf399ebc5c742aaf677
eunlee <eklee2057@naver.com>
2025-09-06 22:09:31 +0900

    fix: 스키마로 리팩토링 된 인터페이스 제거 / DP-318

--- src/features/map/types/cafe.ts
+++ src/features/map/types/cafe.ts
@@ -4,30 +4,6 @@ import type { BaseMapItem } from './common';
  * 카페 관련 타입 정의
  */
 
-/** 개별 리뷰 */
-export interface CafeReview {
-  userId: number;
-  nickname: string;
-  profileImageUrl: string;
-  rating: number;
-  cafeReviewTag: string[];
-  createdAt: string;
-  updatedAt: string;
-}
-
-// TODO: Response는 추후 별도의 파일로 분리 예정
-export interface CafeReviewResponse {
-  cafeId: number;
-  title: string;
-  pagination: {
-    currentPage: number;
-    pageSize: number;
-    totalPages: number;
-    totalItems: number;
-  };
-  cafeReviews: CafeReview[];
-}
-
 /** 지도상의 추천 장소 정보 */
 export interface CafeMapItem extends BaseMapItem {
   cafeId: number;
