ebdba784d89c1a41b16fc712a64bdd4b76437be3
eunlee <eklee2057@naver.com>
2025-09-07 21:27:39 +0900

    fix: 플레이어의 프로필 맵을 조회할 때, 해당 사용자의 프로필에 설정된 location 기반으로 조회하도록 수정 / DP-318

--- src/features/map/components/ProfileMap/ProfileMap.tsx
+++ src/features/map/components/ProfileMap/ProfileMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { Map, MapMarker, useKakaoLoader } from 'react-kakao-maps-sdk';
 
@@ -9,20 +9,18 @@ import styles from './ProfileMap.module.scss';
 
 interface ProfileMapProps {
   playerId: number;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
 }
 
-const ProfileMap = ({ playerId }: ProfileMapProps) => {
+const ProfileMap = ({ playerId, location }: ProfileMapProps) => {
   // 지도의 정보를 담는 ref
   const mapRef = useRef<kakao.maps.Map>(null);
 
   // 지도 사각 영역에 대한 정보
   const [mapBounds, setMapBounds] = useState<MapBounds | null>(null);
-
-  // 사용자 위치 좌표 상태 - 기본값 서울시청으로 설정
-  const [userLocation, setUserLocation] = useState<{
-    lat: number;
-    lng: number;
-  }>({ lat: 37.5665, lng: 126.978 });
 
   // 프로필 지도 데이터 조회
   const { data: profileMapData, isError } = useGetProfileMap({
@@ -30,26 +28,6 @@ const ProfileMap = ({ playerId }: ProfileMapProps) => {
     mapBounds,
     enabled: !!mapBounds && !!playerId,
   });
-
-  // 세션 스토리지에서 사용자 정보 가져오기
-  useEffect(() => {
-    try {
-      const userDataString = sessionStorage.getItem('user');
-      if (userDataString) {
-        const userData = JSON.parse(userDataString);
-        if (userData.location && userData.location.latitude && userData.location.longitude) {
-          setUserLocation({
-            lat: userData.location.latitude,
-            lng: userData.location.longitude,
-          });
-        }
-      }
-    } catch (error) {
-      console.error('세션 스토리지에서 사용자 위치 정보를 가져오는 중 오류 발생:', error);
-      // 오류 발생 시 기본 위치 사용 (서울시청)
-      setUserLocation({ lat: 37.5665, lng: 126.978 });
-    }
-  }, []);
 
   // 지도가 로드된 후 mapBounds 설정
   const handleMapLoad = () => {
@@ -76,7 +54,7 @@ const ProfileMap = ({ playerId }: ProfileMapProps) => {
     <div className={styles.container}>
       <Map
         className={styles.map}
-        center={userLocation}
+        center={{ lat: location.latitude, lng: location.longitude }}
         draggable={false}
         zoomable={false}
         ref={mapRef}
