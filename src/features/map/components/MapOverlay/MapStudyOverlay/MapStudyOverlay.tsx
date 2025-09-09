7cc16e93219924f4892e284ea1f44da3de9ac4bd
eunlee <eklee2057@naver.com>
2025-09-09 21:24:32 +0900

    feat: 스터디 오버레이 상세보기 클릭시 스터디 상세보기 페이지로 연결 / DP-318

--- src/features/map/components/MapOverlay/MapStudyOverlay/MapStudyOverlay.tsx
+++ src/features/map/components/MapOverlay/MapStudyOverlay/MapStudyOverlay.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import Button from '@/components/Button/Button';
 import { useGetStudyOverlay } from '@/features/map/hooks/useGetOverlay';
 
@@ -13,6 +15,8 @@ interface StudyOverlayProps {
 }
 
 const MapStudyOverlay: React.FC<StudyOverlayProps> = ({ id, onOverlayClose }) => {
+  const nav = useNavigate();
+
   const { data: response, isLoading, isError } = useGetStudyOverlay(id);
 
   if (isLoading) {
@@ -70,6 +74,9 @@ const MapStudyOverlay: React.FC<StudyOverlayProps> = ({ id, onOverlayClose }) =>
                 fontWeight="var(--font-weight-regular)"
                 radius="xxsm"
                 padding="4px 10px"
+                onClick={() => {
+                  nav(`/detail/study/${id}`);
+                }}
               >
                 상세보기
               </Button>
