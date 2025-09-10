cc58437dee7dc81b0796111ab978921eb23db9fa
Jammanb0 <151705970+Jammanb0@users.noreply.github.com>
2025-09-11 04:06:25 +0900

    feat: main 페이지 생성 / DP-360

---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,13 @@
+import styles from './MainPage.module.scss';
+
+const MainPage = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>MainPage</div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
