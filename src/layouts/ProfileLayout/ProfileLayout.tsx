45fbb413727bca3c985c0b3a750d8a95af7c9b61
ssoogit <150372021+ssoogit@users.noreply.github.com>
2025-09-13 04:30:58 +0900

    feat(sidebar): 비로그인시 사이드바 제거 / DP-395

--- src/layouts/ProfileLayout/ProfileLayout.tsx
+++ src/layouts/ProfileLayout/ProfileLayout.tsx
@@ -4,17 +4,20 @@ import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute';
 import Header from '@/features/Header/components/Header';
 import SubHeader from '@/features/Header/components/SubHeader';
 import Sidebar from '@/features/Sidebar/components/Sidebar';
+import { useAuthStore } from '@/stores/authStore';
 
 import styles from './ProfileLayout.module.scss';
 
 const ProfileLayout = () => {
+  const { isLoggedIn } = useAuthStore();
+
   return (
     <ProtectedRoute requirePreference={true}>
       <div className={styles.layoutContainer}>
         <Header />
         <SubHeader />
-        <Sidebar />
-        <div className={styles.contentContainer}>
+        {isLoggedIn && <Sidebar />}
+        <div className={`${styles.contentContainer} ${!isLoggedIn ? styles.noSidebar : ''}`}>
           <Outlet />
         </div>
       </div>
