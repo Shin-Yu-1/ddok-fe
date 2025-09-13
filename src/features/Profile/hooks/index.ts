e8afd2fbccd016c538b3e3e09dcc41c1b3ae2741
ssoogit <150372021+ssoogit@users.noreply.github.com>
2025-09-14 04:08:31 +0900

    feat(profile): 사용하지 않는 훅 제거 / DP-411

--- src/features/Profile/hooks/index.ts
+++ src/features/Profile/hooks/index.ts
@@ -1,6 +1,5 @@
 export { useProfileData } from './useProfileData';
 export { useProfileEdit } from './useProfileEdit';
-export { useShowMore } from './useShowMore';
 export { useChatRequest } from './useChatRequest';
 export { useInfiniteLoad } from './useInfiniteLoad';
 export { useProfileModals } from './useProfileModals';
