9c54dc3b417cabea47df77ba282d5fd7754bd463
Shin-Yu-1 <shingyu.user@gmail.com>
2025-09-13 17:30:10 +0900

    fix(main): WebSocketProvider 추가 / DP-399

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import { WebSocketProvider } from '@/utils/ws/WebSocketProvider';
+
 import { router } from './router/router';
+
 import './styles/index.scss';
 
 const queryClient = new QueryClient();
@@ -12,7 +15,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <WebSocketProvider>
+        <RouterProvider router={router} />
+      </WebSocketProvider>
     </QueryClientProvider>
   </StrictMode>
 );
